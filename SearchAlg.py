0b9df17505fa947677b0260d42fb3b29b6f4e686
cyberbush <bush0386@vandals.uidaho.edu>
2021-05-24 18:51:02 -0600

    Added A* and Greedy

--- SearchAlg.py
+++ SearchAlg.py
@@ -1,43 +1,81 @@
 # This is a Python program for CS 470
-# Implementing algorithms for searches
+# Implementing search algorithms for pathfinding
 
 import random
 
+# Read in map from file and save values
+file = "map.txt"
+with open(file) as f:
+    lines = f.readlines()
+
+# Intialize values
+Width = lines[0][:2]
+Height = lines[0][3:5]
+StartX = lines[1][:1]
+StartY = lines[1][2:3]
+GoalX = lines[2][:1]
+GoalY = lines[2][2:4]
+
+basemap = lines[3:]
+#create copies of basemap
+pathmap = [] # will contain found path from start to goal
+for n in basemap:
+    pathmap.append(n)
+exploredmap = [] # will contain all explored cells
+for n in basemap:
+    exploredmap.append(n)
+
+# A cell represents an individual part of the map
 class cell:
-    def __init__(self, x, y):
+    def __init__(self, x, y, cost):
         self.x = x
         self.y = y
-        self.cost = random.randint(10,99)
+        self.cost = cost
         self.costtoreach = 0
-        self.estimatedcosttogoal = 0
+        self.estimatedcosttogoal = 0 #heuristic
+        self.length = 1
         self.parent = None
-    # print
-    # equals 
-    # overload < > to use sort()
+
+    # Sort different based on search used
     def __lt__(self, other):
-        if(self.costtoreach < other.costtoreach):
-            return True
+        #least cost
+        #if(self.costtoreach < other.costtoreach):
+        #    return True
+        #A*
+        #if((self.costtoreach+self.estimatedcosttogoal) < (other.costtoreach+other.estimatedcosttogoal)):
+        #    return True
+        #Greedy
+        if(self.estimatedcosttogoal < other.estimatedcosttogoal):
+           return True
+        
         return False
     
     def printPath(self):
         if(self.parent != None):
             self.parent.printPath()
         print(self.cost)
+    
+    def fillMapPath(self):
+        if(self.parent != None):
+            self.parent.fillMapPath()
+        pathmap[self.y] = pathmap[self.y][:self.x] + '*' + pathmap[self.y][(self.x)+1:]
+    
+    def distance(self):
+        # |x-x'| + |y-y'|
+        return abs(int(GoalX)-self.x) + abs(int(GoalY)-self.y)
 
-#creating map
-map = []
-for x in range (0,7):
-    r = []
-    for y in range (0,7):
-        c = cell(y,x)
-        r.append(c)
-    map.append(r) 
 
-def printMap(): 
+# Function definitions
+def printCostMap(): 
     for r in map:
         for c in r:
             print(c.cost, end = " ")
         print()
+
+def printMap(obj):
+    for n in obj:
+        print(n, end = "")
+    print()
 
 def searchList(thisList, item):
     for i in thisList:
@@ -47,58 +85,123 @@ def searchList(thisList, item):
 
 def printList(L):
     for i in L:
-        print(i.cost, end = " ")
+        #print(i.costtoreach, end = " ") # least cost
+        #print(i.costtoreach+i.estimatedcosttogoal, end = " ") # A*
+        print(i.estimatedcosttogoal, end = " ") # greedy
     print()
 
+def findCost(terrain):
+    switcher={
+        'R':1,  #road 
+        'f':2,  #field
+        'F':4,  #forest
+        'h':5,  #hills
+        'r':7,  #river
+        'M':10, #mountains
+        'W':333   #water
+    }
+    return switcher.get(terrain)
+
+def avg():
+    return (1+2+4+5+7+10)/6
+
+
+#creating map
+map = []
+for y in range (0,int(Height)):
+    r = []
+    for x in range (0,int(Width)):
+        cost = findCost(basemap[y][x])
+        c = cell(x,y, cost)
+        r.append(c)
+    map.append(r) 
 
 #intialize 
 openlist = []
 closedlist = []
-goal = map[5][5]
+goal = map[int(GoalY)][int(GoalX)]
 
 def search():
-    start = map[0][3]
+    start = map[int(StartY)][int(StartX)]
     start.costtoreach = start.cost
     start.parent = None
+    start.estimatedcosttogoal = start.distance() * avg() # heuristic
     openlist.append(start)
     while(len(openlist) > 0):
-        #Least cost search
-        #openlist.sort()
+        # Breadth-first no sorting
+        # Least cost sort by total cost
+        # A* sort by total cost + estimated cost
+        # Greedy sort by estimated cost (heuristic)
+        openlist.sort()
         current = openlist.pop(0)
+
+        #check if the goal has been reached
         if(current == goal):
-            print("Success")
-            goal.printPath()
+            #goal.printPath()
+            goal.fillMapPath()
+            print("Total Cost: ", goal.costtoreach)
+            print("Total Length: ", goal.length)
+            print()
+            print("Open List:")
+            printList(openlist)
+            print()
             break
-        closedlist.append(current)
-        # find each neighbor of current 
-        # if a neighbor is not on open or closed list
-        # add it to the openlist
+        #mark on explored map
+        exploredmap[current.y] = exploredmap[current.y][:current.x] + '-' + exploredmap[current.y][(current.x)+1:]
+        closedlist.append(current) #add to closed list
+        # Find each neigbor of current and add to openlist if hasn't been searched yet
         if(current.y > 0): #top neighbor
             neighbor = map[current.y-1][current.x]
+            #check if neighbor is water and place on closed list
+            if(neighbor.cost == 333):
+                closedlist.append(neighbor)
             #check openlist to see if neighbor is the lower cost neighbor
             if(not searchList(openlist, neighbor) and not searchList(closedlist, neighbor)):
                 neighbor.costtoreach = current.costtoreach + neighbor.cost
+                neighbor.estimatedcosttogoal = neighbor.distance() * avg() 
+                neighbor.length = current.length + neighbor.length
                 neighbor.parent = current
                 openlist.append(neighbor)
-        if(current.y < len(map[0])-1): #bottom neighbor
+        if(current.y < len(map)-1): #bottom neighbor
             neighbor = map[current.y+1][current.x]
+            #check if neighbor is water and place on closed list
+            if(neighbor.cost == 333):
+                closedlist.append(neighbor)
             if(not searchList(openlist, neighbor) and not searchList(closedlist, neighbor)):
                 neighbor.costtoreach = current.costtoreach + neighbor.cost
+                neighbor.estimatedcosttogoal = neighbor.distance() * avg() 
+                neighbor.length = current.length + neighbor.length
                 neighbor.parent = current
                 openlist.append(neighbor)
         if(current.x > 0): #left neighbor
             neighbor = map[current.y][current.x-1]
+            #check if neighbor is water and place on closed list
+            if(neighbor.cost == 333):
+                closedlist.append(neighbor)
             if(not searchList(openlist, neighbor) and not searchList(closedlist, neighbor)):
                 neighbor.costtoreach = current.costtoreach + neighbor.cost
+                neighbor.estimatedcosttogoal = neighbor.distance() * avg() 
+                neighbor.length = current.length + neighbor.length
                 neighbor.parent = current
                 openlist.append(neighbor)
-        if(current.x < len(map)-1): #right neighbor
+        if(current.x < len(map[0])-1): #right neighbor
             neighbor = map[current.y][current.x+1]
+            #check if neighbor is water and place on closed list
+            if(neighbor.cost == 333):
+                closedlist.append(neighbor)
             if(not searchList(openlist, neighbor) and not searchList(closedlist, neighbor)):
                 neighbor.costtoreach = current.costtoreach + neighbor.cost
+                neighbor.estimatedcosttogoal = neighbor.distance() * avg() 
+                neighbor.length = current.length + neighbor.length
                 neighbor.parent = current
                 openlist.append(neighbor)
-        
-#print map 
-printMap()
+
+#Driver Code
+#printcostMap()
+print("Starting Map:")
+printMap(basemap)
 search()
+print("Path Found:")
+printMap(pathmap)
+print("Explored Map:")
+printMap(exploredmap)
